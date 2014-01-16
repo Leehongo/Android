ea6cf5e4b989f306a46be38714f878b28133c125
JOHNLEE MANGAHAS <johnleemangahas@yahoo.com>
2014-01-16 08:48:19 +0800

    new images

---
+++ src/com/game/bugattong/model/Objects.java
@@ -0,0 +1,46 @@
+package com.game.bugattong.model;
+
+public class Objects {
+	
+//	private int level;
+//	private int position;
+//	
+//	public Objects(int level, int postion) {
+//		this.setLevel(level);
+//		this.setPosition(postion);
+//	}
+//	
+//	
+//	public int getLevel() {
+//		return level;
+//	}
+//	public void setLevel(int level) {
+//		this.level = level;
+//	}
+//	public int getPosition() {
+//		return position;
+//	}
+//	public void setPosition(int position) {
+//		this.position = position;
+//	}
+	
+	private int objectImage;
+	
+	public Objects(int obj) {
+		this.setObjectImage(obj);
+	}
+
+	public int getObjectImage() {
+		return objectImage;
+	}
+
+	public void setObjectImage(int objectImage) {
+		this.objectImage = objectImage;
+	}
+	
+	
+	
+	
+	
+
+}
